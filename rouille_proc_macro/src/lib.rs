8d4c35679a12f22deb6550e704eea70103e826fe
Johan Planchon <johan.planchon@simplx.fr>
2022-10-24 13:31:09 +0200

    Rewritten rouille using rouille

--- rouille_proc_macro/src/lib.rs
+++ rouille_proc_macro/src/lib.rs
@@ -1,125 +1,127 @@
-use proc_macro::{Group, Ident, TokenStream, TokenTree};
+rouille::rouille! {
+    utilisons macro_procédurale::{Groupe, Identifiant, FluxDeJetons, ArbreDeJetons};
 
-fn replace_ident(ident: Ident) -> Option<TokenTree> {
-    let ident_str = ident.to_string();
+    fonction remplacer_identifiant(identifiant: Identifiant) -> PeutÊtre<ArbreDeJetons> {
+        soit identifiant_chaine_de_charactères = identifiant.vers_la_chaine_de_charactères();
 
-    let new_str = match ident_str.as_str() {
-        "Arf" => "Err",
-        "Bien" => "Ok",
-        "Chaine" => "String",
-        "Dictionnaire" => "HashMap",
-        "Défaut" => "Default",
-        "Erreur" => "Error",
-        "PeutÊtre" => "Option",
-        "Quelque" => "Some",
-        "Rien" => "None",
-        "Résultat" => "Result",
-        "Soi" => "Self",
-        "affiche" => "println",
-        "arrête" => "break",
-        "asynchrone" => "async",
-        "attend" => "await",
-        "boucle" => "loop",
-        "bouge" => "move",
-        "cagette" => "crate",
-        "code_inaccessible" => "unreachable_code",
-        "comme" => "as",
-        "constant" => "const",
-        "convention" => "trait",
-        "dangereux" => "unsafe",
-        "de" => "in",
-        "depuis" => "from",
-        "dynamique" => "dyn",
-        "déballer" => "unwrap",
-        "défaut" => "default",
-        "en_réf" => "as_ref",
-        "es" => "io",
-        "externe" => "extern",
-        "faux" => "false",
-        "fonction" => "fn",
-        "génial" => "super",
-        "insérer" => "insert",
-        "lire" => "get",
-        "légal" => "allow",
-        "merde" | "calisse" | "oups" => "panic",
-        "module" => "mod",
-        "mutable" => "mut",
-        "nouveau" => "new",
-        "où" => "where",
-        "pour" => "for",
-        "prendre_ou_insérer_avec" => "get_or_insert_with",
-        "principale" => "main",
-        "public" => "pub",
-        "que" => None?,
-        "renvoie" => "return",
-        "réalisation" => "impl",
-        "réf" => "ref",
-        "selon" => "match",
-        "si" => "if",
-        "sinon" => "else",
-        "soi" => "self",
-        "soit" => "let",
-        "statique" => "static",
-        "structure" => "struct",
-        "suppose" => "expect",
-        "tant" => "while",
-        "utilisons" => "use",
-        "vers" => "into",
-        "vrai" => "true",
-        "énumération" => "enum",
-        "Groupe" => "Group",
-        "Identifiant" => "Ident",
-        "FluxDeJetons" => "TokenStream",
-        "ArbreDeJetons" => "TokenTree",
-        "vers_la_chaine_de_charactères" => "to_string",
-        "en_tant_que_chaine_de_charactère" => "as_str",
-        "portée" => "span",
-        "Tableau" => "Vec",
-        "flux" => "stream",
-        "pousser" => "push",
-        "étendre" => "extend",
-        "délimiteur" => "delimiter",
-        "Ponctuation" => "Punct",
-        "Litéral" => "Literal",
-        "macro_procédurale" => "proc_macro",
-        _ => &ident_str,
-    };
+        soit nouvelle_chaine_de_charactères = selon identifiant_chaine_de_charactères.as_str() {
+            "Arf" => "Err",
+            "Bien" => "Ok",
+            "Chaine" => "String",
+            "Dictionnaire" => "HashMap",
+            "Défaut" => "Default",
+            "Erreur" => "Error",
+            "PeutÊtre" => "Option",
+            "Quelque" => "Some",
+            "Rien" => "None",
+            "Résultat" => "Result",
+            "Soi" => "Self",
+            "affiche" => "println",
+            "arrête" => "break",
+            "asynchrone" => "async",
+            "attend" => "await",
+            "boucle" => "loop",
+            "bouge" => "move",
+            "cagette" => "crate",
+            "code_inaccessible" => "unreachable_code",
+            "comme" => "as",
+            "constant" => "const",
+            "convention" => "trait",
+            "dangereux" => "unsafe",
+            "de" => "in",
+            "depuis" => "from",
+            "dynamique" => "dyn",
+            "déballer" => "unwrap",
+            "défaut" => "default",
+            "en_réf" => "as_ref",
+            "es" => "io",
+            "externe" => "extern",
+            "faux" => "false",
+            "fonction" => "fn",
+            "génial" => "super",
+            "insérer" => "insert",
+            "lire" => "get",
+            "légal" => "allow",
+            "merde" | "calisse" | "oups" => "panic",
+            "module" => "mod",
+            "mutable" => "mut",
+            "nouveau" => "new",
+            "où" => "where",
+            "pour" => "for",
+            "prendre_ou_insérer_avec" => "get_or_insert_with",
+            "principale" => "main",
+            "public" => "pub",
+            "que" => None?,
+            "renvoie" => "return",
+            "réalisation" => "impl",
+            "réf" => "ref",
+            "selon" => "match",
+            "si" => "if",
+            "sinon" => "else",
+            "soi" => "self",
+            "soit" => "let",
+            "statique" => "static",
+            "structure" => "struct",
+            "suppose" => "expect",
+            "tant" => "while",
+            "utilisons" => "use",
+            "vers" => "into",
+            "vrai" => "true",
+            "énumération" => "enum",
+            "Groupe" => "Group",
+            "Identifiant" => "Ident",
+            "FluxDeJetons" => "TokenStream",
+            "ArbreDeJetons" => "ArbreDeJetons",
+            "vers_la_chaine_de_charactères" => "to_string",
+            "en_tant_que_chaine_de_charactère" => "as_str",
+            "portée" => "span",
+            "Tableau" => "Vec",
+            "flux" => "stream",
+            "pousser" => "push",
+            "étendre" => "extend",
+            "délimiteur" => "delimiter",
+            "Ponctuation" => "Punct",
+            "Litéral" => "Literal",
+            "macro_procédurale" => "proc_macro",
+            _ => &identifiant_chaine_de_charactères,
+        };
 
-    let new_ident = Ident::new(new_str, ident.span());
-    Some(TokenTree::Ident(new_ident))
-}
+        soit nouvel_identifiant = Identifiant::nouveau(nouvelle_chaine_de_charactères, identifiant.portée());
+        Quelque(ArbreDeJetons::Identifiant(nouvel_identifiant))
+    }
 
-fn replace_tree(tok: TokenTree, out: &mut Vec<TokenTree>) {
-    match tok {
-        TokenTree::Group(group) => {
-            let mut group_elem = Vec::new();
-            replace_stream(group.stream(), &mut group_elem);
-            let mut new_stream = TokenStream::new();
-            new_stream.extend(group_elem);
-            out.push(TokenTree::Group(Group::new(group.delimiter(), new_stream)));
-        }
-        TokenTree::Ident(ident) => {
-            if let Some(ident) = replace_ident(ident) {
-                out.push(ident);
+    fonction remplacer_arbre(jeton: ArbreDeJetons, sortie: &mutable Tableau<ArbreDeJetons>) {
+        selon jeton {
+            ArbreDeJetons::Groupe(groupe) => {
+                soit mutable groupe_elements = Tableau::nouveau();
+                remplacer_le_flux(groupe.flux(), &mutable groupe_elements);
+                soit mutable nouveau_flux = FluxDeJetons::nouveau();
+                nouveau_flux.étendre(groupe_elements);
+                sortie.pousser(ArbreDeJetons::Groupe(Groupe::nouveau(groupe.délimiteur(), nouveau_flux)));
+            }
+            ArbreDeJetons::Identifiant(identifiant) => {
+                si soit Quelque(identifiant) = remplacer_identifiant(identifiant) {
+                    sortie.pousser(identifiant);
+                }
+            }
+            ArbreDeJetons::Ponctuation(..) | ArbreDeJetons::Litéral(..) => {
+                sortie.pousser(jeton);
             }
         }
-        TokenTree::Punct(..) | TokenTree::Literal(..) => {
-            out.push(tok);
+    }
+
+    fonction remplacer_le_flux(arbre_de_jetons: FluxDeJetons, sortie: &mutable Tableau<ArbreDeJetons>) {
+        pour jeton de arbre_de_jetons {
+            remplacer_arbre(jeton, sortie)
         }
     }
-}
 
-fn replace_stream(ts: TokenStream, out: &mut Vec<TokenTree>) {
-    for tok in ts {
-        replace_tree(tok, out)
+    #[macro_procédurale]
+    public fonction rouille(élément: FluxDeJetons) -> FluxDeJetons {
+        soit mutable retourné = Tableau::nouveau();
+        remplacer_le_flux(élément, &mutable retourné);
+        soit mutable sortie = FluxDeJetons::nouveau();
+        sortie.étendre(retourné);
+        sortie
     }
-}
-
-#[proc_macro]
-pub fn rouille(item: TokenStream) -> TokenStream {
-    let mut returned = Vec::new();
-    replace_stream(item, &mut returned);
-    let mut out = TokenStream::new();
-    out.extend(returned);
-    out
 }
